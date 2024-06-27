59c563cc29e8598a19b8bb6f3eb94bb428975907
GitAniket98 <aniketchaudhary9838@gmail.com>
2024-06-27 08:20:47 +0530

    Changed whole codebase

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
 
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
-  networks: {
-    sepolia: {
-      url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts: [`0x${process.env.PRIVATE_KEY}`],
-    },
-  },
+  solidity: "0.8.24",
 };
