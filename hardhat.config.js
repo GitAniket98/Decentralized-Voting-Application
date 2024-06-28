8eda97f48c7f063dfda770e64ea24a34af784388
GitAniket98 <aniketchaudhary9838@gmail.com>
2024-06-28 23:04:50 +0530

    changes to hardhat.config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
 
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+module.exports = {
   solidity: "0.8.4",
   networks: {
     hardhat: {},
-    : {
+    rinkeby: {
       url: `https://rinkeby.infura.io/v3/YOUR_INFURA_PROJECT_ID`,
-      accounts: [`0x${YOUR_PRIVATE_KEY}`]
-    }
-  }
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
 };
